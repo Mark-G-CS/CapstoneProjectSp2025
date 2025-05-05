813311393884d0386ed13361ae0c54745d92e910
Matthew <thenewcom56@outlook.com>
2025-05-04 18:23:17 -0700

    rely on auto‑init

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,8 @@ import 'package:flutter/material.dart';
 import 'firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
 
-void main() async{
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp( //initialize Firebase
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
   runApp(const MyApp());
 }
 
